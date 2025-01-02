ed484d7a73d01a602cfd93a9b30e6b521be80e6b
damomaiden <damomaiden@gmail.com>
2025-01-02 13:28:33 +0000

    Updated Music and hit audio
    Removed drums from original track, used the drum beat on successful hits

--- Assets/Scripts/RandomSample.cs
+++ Assets/Scripts/RandomSample.cs
@@ -1,21 +1,36 @@
-using NUnit.Framework;
 using UnityEngine;
 
 public class RandomSample : MonoBehaviour
 {
-
-    [SerializeField] AudioClip[] clips;
-    AudioSource myAudioSource;
+    [SerializeField] private AudioClip[] breakableClips; // Audio clips for breakable objects
+    [SerializeField] private AudioClip[] sliceableClips; // Audio clips for sliceable objects
+    private AudioSource myAudioSource;
 
     private void Start()
     {
-     myAudioSource = GetComponent<AudioSource>();   
+        // Get the AudioSource component attached to this GameObject
+        myAudioSource = GetComponent<AudioSource>();
     }
+
     private void OnTriggerEnter(Collider other)
     {
-        myAudioSource.clip = clips[Random.Range(0, clips.Length-1)];
-        myAudioSource.Play();
+        // Check if the other object has the correct tag and play corresponding audio
+        if (other.CompareTag("Breakable"))
+        {
+            PlayRandomClip(breakableClips);
+        }
+        else if (other.CompareTag("Sliceable"))
+        {
+            PlayRandomClip(sliceableClips);
+        }
     }
 
-
+    private void PlayRandomClip(AudioClip[] clips)
+    {
+        if (clips != null && clips.Length > 0)
+        {
+            AudioClip selectedClip = clips[Random.Range(0, clips.Length)];
+            myAudioSource.PlayOneShot(selectedClip);
+        }
+    }
 }
