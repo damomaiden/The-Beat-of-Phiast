e64fe4d7c8f73819be5213ea58315b7f39971c8c
J-o-n-a-t-han <24jonathanlynch@bcfe.ie>
2025-02-13 13:55:48 +0000

    Name System Re-added
    I finished the name system and almost finished the high score system at home, but it didn't save to GitHub. Redoing it.

--- Assets/Scripts/ScoreScripts/NameInput.cs
+++ Assets/Scripts/ScoreScripts/NameInput.cs
@@ -6,6 +6,8 @@ public class NameInput : MonoBehaviour
     [SerializeField] HighScore_SO highScore; //Remebers the highest score from all previous play sessions
     [SerializeField] TextMeshPro text; //will write name here
     [SerializeField] int nameNumber = 10101; //Name is calculated on a scale of 010101 to 262626
+    [SerializeField] int midNumber = 101; //For calculating changes made to the middle number
+    [SerializeField] int smallNumber = 1; //For calculating changes made to the smaller number
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,8 +27,8 @@ public class NameInput : MonoBehaviour
 
     public void DownFirst()
     {
-        nameNumber = nameNumber - 100;
-        if (nameNumber <= 10000)
+        nameNumber = nameNumber - 10000;
+        if (nameNumber < 10000)
         {
             nameNumber = nameNumber + 260000;
         }
@@ -35,25 +37,45 @@ public class NameInput : MonoBehaviour
     public void UpSecond()
     {
         nameNumber = nameNumber + 100;
-        if (nameNumber >= 2700)
+        nameNumber = midNumber + 100;
+        if (midNumber >= 2700)
         {
-            nameNumber = nameNumber - 250000;
+            nameNumber = nameNumber - 2500;
+            midNumber = midNumber - 2500;
         }
     }
 
     public void DownSecond()
     {
         nameNumber = nameNumber - 100;
+        nameNumber = midNumber - 100;
+        if (midNumber < 100)
+        {
+            nameNumber = nameNumber + 2600;
+            midNumber = midNumber + 2600;
+        }
     }
 
     public void UpThird()
     {
         nameNumber = nameNumber + 1;
+        nameNumber = smallNumber + 1;
+        if (smallNumber >= 27)
+        {
+            nameNumber = nameNumber - 25;
+            smallNumber = smallNumber - 25;
+        }
     }
 
     public void DownThird()
     {
         nameNumber = nameNumber - 1;
+        nameNumber = smallNumber + 1;
+        if (smallNumber < 1)
+        {
+            nameNumber = nameNumber + 26;
+            smallNumber = smallNumber + 26;
+        }
     }
 
     // Update is called once per frame
