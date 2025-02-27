1f080a9a0c2005733b85e2b74884d79f891fce48
J-o-n-a-t-han <24jonathanlynch@bcfe.ie>
2025-02-27 15:36:03 +0000

    Particles and bits
    Particles for splashing, as well as (hopefully) water fix. Going to work on coordinating the splash effects to An Phiast's feet during the dance animation.

---
+++ Assets/Scripts/ScoreScripts/PhiastDance.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PhiastDance : MonoBehaviour
+{
+    public GameObject SplishSplash; //This is the variable to control the "Splash" particles
+
+    // This script is to control the particle effects so that when An Phiast moves in the water there is a splash
+    public void LeftSplash() //Left leg's splash control
+    {
+        //Neither are hooked up yet
+    }
+
+    public void RightSplash() //Right leg's splash control
+    {
+        
+    }
+}
