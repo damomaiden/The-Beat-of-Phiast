c2d69201a1c288ad4ef94cc03d27eb301293b982
J-o-n-a-t-han <24jonathanlynch@bcfe.ie>
2025-02-27 15:44:59 +0000

    Particles added to dance (early)
    Started scripting for the particles to connect to the dancing, right now it just turns on though

--- Assets/Scripts/ScoreScripts/PhiastDance.cs
+++ Assets/Scripts/ScoreScripts/PhiastDance.cs
@@ -4,14 +4,20 @@ public class PhiastDance : MonoBehaviour
 {
     public GameObject SplishSplash; //This is the variable to control the "Splash" particles
 
+    private void Start()
+    {
+        SplishSplash.SetActive(false);
+    }
+
     // This script is to control the particle effects so that when An Phiast moves in the water there is a splash
     public void LeftSplash() //Left leg's splash control
     {
-        //Neither are hooked up yet
+        SplishSplash.SetActive(true);
+        
     }
 
     public void RightSplash() //Right leg's splash control
     {
-        
+        //Neither are hooked up yet
     }
 }
